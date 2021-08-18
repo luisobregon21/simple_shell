ecf2762d1d2892d497ff74093827e4bf51cdc87c
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-08-18 20:29:49 +0000

    added comments on functions that need to be created

--- shell.h
+++ shell.h
@@ -6,6 +6,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-int _putchar(char c);
 
-#endif /* SHELL GUARDS*/
+int _putchar(char c);
+char *userinput(void);
+//strncmp function - Luis
+//2d array converter function - Joahnne
+//input validator function
+//path finder function
+//concatenator function
+//forking function
+//executor
+//The freer
+
+#endif
