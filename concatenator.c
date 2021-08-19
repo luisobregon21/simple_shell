7a4744505f3659910e5d461d3ae39cab0a99e966
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-08-19 18:47:22 +0000

    added concatenator function

---
+++ concatenator.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+ * *concatenator - concatenates two strings to find the right executable path.
+ * @path: the path we are completing.
+ * @userinput: what the user types.
+ *
+ * Return: the path to be executed.
+ */
+
+char *concatenator(char *path, char *userinput)
+{
+	int index, index2;
+
+	for (index = 0; path[index] != '\0'; index++)
+	;
+
+	for (index2 = 0; userinput[index2] != '\0'; index2++)
+	{
+		path[index] = userinput[index2];
+		index++;
+	}
+	return (path);
+}
